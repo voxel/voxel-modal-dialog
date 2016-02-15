a8d770ef327e16b5a830421fc3f0df0fbb6b6d11
deathcap <deathcap@gmx.co.uk>
2016-02-14 19:03:03 -0800

    Port to native ES6

--- dialog.js
+++ dialog.js
@@ -1,16 +1,22 @@
+'use strict';
 
-Modal = require 'voxel-modal'
+const Modal = require('voxel-modal');
 
-class ModalDialog extends Modal
-  constructor: (@game, opts) ->
-    opts ?= {}
-    opts.contents ?= []
+class ModalDialog extends Modal {
+  constructor(game, opts) {
+    super(game, ModalDialog.createDialogContent(opts));
+  }
 
-    if document?
-      # covers the (almost) entire page, for alignment purposes
-      @aligner = document.createElement 'div'
-      @aligner.setAttribute 'class', 'voxel-modal-dialog-aligner'
-      @aligner.setAttribute 'style', '
+  static createDialogContent(opts) {
+    if (opts.contents) opts.contents = [];
+
+    let box;
+
+    if (typeof document !== 'undefined') {
+      // covers the (almost) entire page, for alignment purposes
+      const aligner = document.createElement('div');
+      aligner.setAttribute('class', 'voxel-modal-dialog-aligner');
+      aligner.setAttribute('style', `
         display: flex;
         align-items: center;
         justify-content: center;
@@ -20,27 +26,31 @@ class ModalDialog extends Modal
         height: 90%;
         position: fixed;
         pointer-events: none;
-        '
-        # note: height not 100% to give some room for voxel-inventory-hotbar
+        `)
+        // note: height not 100% to give some room for voxel-inventory-hotbar
 
-      # the overall dialog box element
-      @box = document.createElement 'div'
-      @box.setAttribute 'class', 'voxel-modal-dialog'
-      @box.style.border = '6px outset gray'
-      @box.style.visibility = 'hidden'
-      @box.style.zIndex = 1
-      @box.style.pointerEvents = 'auto' # reset pointer-events:none on parent (to allow clicks through canvas)
-      @box.style.backgroundImage = 'linear-gradient(rgba(255,255,255,0.5) 0%, rgba(255,255,255,0.5) 100%)'
+      // the overall dialog box element
+      box = document.createElement('div');
+      box.setAttribute('class', 'voxel-modal-dialog');
+      box.style.border = '6px outset gray';
+      box.style.visibility = 'hidden';
+      box.style.zIndex = 1;
+      box.style.pointerEvents = 'auto'; // reset pointer-events:none on parent (to allow clicks through canvas)
+      box.style.backgroundImage = 'linear-gradient(rgba(255,255,255,0.5) 0%, rgba(255,255,255,0.5) 100%)';
 
-      for content in opts.contents
-        @box.appendChild(content)
+      for (let content of opts.contents) {
+        box.appendChild(content);
+      }
 
-      @aligner.appendChild @box
-      document.body.appendChild @aligner
+      aligner.appendChild(box);
+      document.body.appendChild(aligner);
+    }
 
-    opts.element = @box
+    opts.element = box
 
-    super game, opts
+    return opts;
+  }
+}
 
 module.exports = ModalDialog
 
